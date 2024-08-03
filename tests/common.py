60d473e32118b9892006b22e36a49fb447241462
Moe <rezaalipour.mohammad@gmail.com>
2024-08-03 18:26:44 +0200

    Remove from tests the items depending on fauxpy installed on pytest

--- tests/common.py
+++ tests/common.py
@@ -1,10 +1,10 @@
-import glob
-import os
-import shutil
+# import glob
+# import os
+# import shutil
 from pathlib import Path
-from typing import List
+# from typing import List
 
-import pathlib
+# import pathlib
 
 
 def getDataPath(directoryName: str, fileName: str) -> Path:
@@ -14,57 +14,43 @@ def getDataPath(directoryName: str, fileName: str) -> Path:
     return dataDir / directoryName / fileName
 
 
-def deleteNoise(projectPath: Path):
-    filesToRemove = [
-        "instrumentationCollectModeExecutedPredicateSequence.txt",
-        "instrumentationCollectModeConfig.txt",
-        "instrumentationCollectModeEvaluationCounter.txt",
-    ]
-
-    for item in filesToRemove:
-        pathItem = projectPath / pathlib.Path(item)
-        if pathItem.exists():
-            pathItem.unlink()
-
-    dirToRemove = projectPath / "FauxPyCollectModeReport"
-    if dirToRemove.exists():
-        shutil.rmtree(dirToRemove)
-
-    reportDirsToRemove = projectPath.parent.rglob("FauxPyReport*")
-    for item in reportDirsToRemove:
-        shutil.rmtree(item)
-
-    pattern = f"{str(projectPath.absolute())}/**/*.pyc"
-    fileList = glob.glob(pattern, recursive=True)
-    for filePath in fileList:
-        if os.path.exists(filePath):
-            os.remove(filePath)
-
-
-# def runCommand(cmd: List[str], workingDir: str):
-#     import subprocess
-#     execOut = subprocess.Popen(cmd,
-#                                stdout=subprocess.PIPE,
-#                                stderr=subprocess.PIPE,
-#                                universal_newlines=True,
-#                                cwd=workingDir)
+# def deleteNoise(projectPath: Path):
+#     filesToRemove = [
+#         "instrumentationCollectModeExecutedPredicateSequence.txt",
+#         "instrumentationCollectModeConfig.txt",
+#         "instrumentationCollectModeEvaluationCounter.txt",
+#     ]
 #
-#     for line in execOut.stdout:
-#         print(line.replace('\n', ''))
-#     for line in execOut.stderr:
-#         print(line.replace('\n', ''))
+#     for item in filesToRemove:
+#         pathItem = projectPath / pathlib.Path(item)
+#         if pathItem.exists():
+#             pathItem.unlink()
+#
+#     dirToRemove = projectPath / "FauxPyCollectModeReport"
+#     if dirToRemove.exists():
+#         shutil.rmtree(dirToRemove)
+#
+#     reportDirsToRemove = projectPath.parent.rglob("FauxPyReport*")
+#     for item in reportDirsToRemove:
+#         shutil.rmtree(item)
+#
+#     pattern = f"{str(projectPath.absolute())}/**/*.pyc"
+#     fileList = glob.glob(pattern, recursive=True)
+#     for filePath in fileList:
+#         if os.path.exists(filePath):
+#             os.remove(filePath)
 
 
-def runCommand(command: List[str], workingDir: str):
-    import subprocess
-
-    execOut = subprocess.run(
-        command,
-        stdout=subprocess.PIPE,
-        stderr=subprocess.PIPE,
-        universal_newlines=True,
-        cwd=workingDir,
-    )
-
-    print(execOut.stdout)
-    print(execOut.stderr)
+# def runCommand(command: List[str], workingDir: str):
+#     import subprocess
+#
+#     execOut = subprocess.run(
+#         command,
+#         stdout=subprocess.PIPE,
+#         stderr=subprocess.PIPE,
+#         universal_newlines=True,
+#         cwd=workingDir,
+#     )
+#
+#     print(execOut.stdout)
+#     print(execOut.stderr)
--- tests/data/predicate_switching/no_exceptions/tests/test_main.py
+++
@@ -1,7 +0,0 @@
-from src.main import mainFunction
-
-
-def test_mainFunction():
-    a = 2
-    b = mainFunction(a)
-    assert b == -1
