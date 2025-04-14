60381b77d0e7fb48e040eb6f44fc964279313e49
Moe <rezaalipour.mohammad@gmail.com>
2025-04-14 16:32:43 +0200

    Add docstrings to FauxpySessionType class

--- fauxpy/session_lib/fauxpy_session_type.py
+++ fauxpy/session_lib/fauxpy_session_type.py
@@ -2,5 +2,11 @@ from enum import Enum
 
 
 class FauxpySessionType(Enum):
+    """Represents the type of a FauxPy session.
+
+    Attributes:
+        FaultLocalization: A session where FauxPy is actively used for fault localization.
+        FauxpyNotCalled: A session where FauxPy was not invoked during the test run.
+    """
     FaultLocalization = 1
     FauxpyNotCalled = 2
