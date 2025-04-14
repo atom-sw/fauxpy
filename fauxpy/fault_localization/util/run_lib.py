f2321583f56f59363382ac2e1822a17b7c04e13f
Moe <rezaalipour.mohammad@gmail.com>
2025-04-14 16:36:57 +0200

    Refactor run lib for better printing

--- fauxpy/fault_localization/util/run_lib.py
+++ fauxpy/fault_localization/util/run_lib.py
@@ -2,14 +2,16 @@ from subprocess import TimeoutExpired
 from typing import List, Optional
 import subprocess
 
+from fauxpy.session_lib.fauxpy_printer import fl_print
+
 
 class CommandRunner:
     @staticmethod
     def run_command(
         command: List[str], working_dir: str, process_timeout: Optional[float]
     ):
-        print("--------------------Subprocess Begin---------------------")
-        print("Command to run: ", " ".join(command))
+        fl_print.detailed("--------------------Subprocess Begin---------------------")
+        fl_print.detailed("Command to run: ", " ".join(command))
         exec_out = None
         if process_timeout is None:
             exec_out = subprocess.run(
@@ -30,10 +32,10 @@ class CommandRunner:
                     timeout=process_timeout,
                 )
             except TimeoutExpired:
-                print("Subprocess timeout")
+                fl_print.detailed("Subprocess timeout")
                 pass
 
         if exec_out is not None:
-            print(exec_out.stdout)
-            print(exec_out.stderr)
-        print("--------------------Subprocess End---------------------")
+            fl_print.detailed(exec_out.stdout)
+            fl_print.detailed(exec_out.stderr)
+        fl_print.detailed("--------------------Subprocess End---------------------")
