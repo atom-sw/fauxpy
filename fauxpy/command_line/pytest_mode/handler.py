233fbbffcff324ca2e7e85102891b1282eaa9001
Moe <rezaalipour.mohammad@gmail.com>
2025-04-14 16:47:18 +0200

    Enhance handler for better reporting

--- fauxpy/command_line/pytest_mode/handler.py
+++ fauxpy/command_line/pytest_mode/handler.py
@@ -1,8 +1,8 @@
 import logging
 from typing import Optional
 
-from fauxpy.command_line.pytest_mode import legacy_input
 from fauxpy.command_line.pytest_mode.pytest_option_manager import PytestOptionManager
+from fauxpy.session_lib.fauxpy_printer import fl_print
 from fauxpy.session_lib.fauxpy_session_type import FauxpySessionType
 from fauxpy.session_lib.fl_family_session import FlFamilySession
 from fauxpy.session_lib.fl_session import FlSession
@@ -16,7 +16,7 @@ class FauxpyPytestModeHandler:
     """
 
     def __init__(self):
-        self._ex_timer = Timer()
+        self._session_timer = Timer()
         self._pytest_option_manager = PytestOptionManager()
         self._fauxpy_session_type = FauxpySessionType.FauxpyNotCalled
         self._session_object: Optional[FlSession] = None
@@ -43,18 +43,13 @@ class FauxpyPytestModeHandler:
             pytest_config
         )
         if self._fauxpy_session_type == FauxpySessionType.FaultLocalization:
-            self._ex_timer.start_timer()
+            self._session_timer.start()
 
             fl_option_manager = self._pytest_option_manager.get_fl_option_manager(
                 pytest_config
             )
 
-            targeted_failing_test_list_legacy = (
-                legacy_input.get_targeted_failing_test_list_legacy(
-                    fl_option_manager.get_targeted_failing_test_list()
-                )
-            )
-
+            self._session_object = fl_option_manager.get_fl_session()
             self._session_file_manager = fl_option_manager.get_session_file_manager()
 
             logging.basicConfig(
@@ -64,17 +59,16 @@ class FauxpyPytestModeHandler:
                 filemode="w",
             )
 
-            self._session_object = fl_option_manager.get_fl_session()
-
-            if targeted_failing_test_list_legacy is not None:
-                print("----------- TARGET FAILING TESTS -----------")
-                for item in targeted_failing_test_list_legacy.get_failing_tests():
-                    print(item)
-                print("----------- TARGET FAILING TEST -----------")
-
-            legacy_input.save_config_info_legacy(
-                pytest_config, self._session_file_manager
-            )
+            if isinstance(self._session_object, FlFamilySession):
+                self._session_file_manager.save_config_to_file(
+                    target_src=fl_option_manager.get_target_src(),
+                    exclude_list=fl_option_manager.get_exclude_list(),
+                    fl_family=fl_option_manager.get_fl_family(),
+                    fl_granularity=fl_option_manager.get_fl_granularity(),
+                    top_n=fl_option_manager.get_top_n(),
+                    targeted_failing_test_list=fl_option_manager.get_targeted_failing_test_list(),
+                    mutation_strategy=fl_option_manager.get_mutation_strategy()
+                )
 
     def runtest_call(self, item):
         """
@@ -106,18 +100,56 @@ class FauxpyPytestModeHandler:
             exitstatus: The exit status of Pytest, indicating success or failure.
         """
         if self._fauxpy_session_type == FauxpySessionType.FaultLocalization:
-            score_entities = self._session_object.terminal_summary(
+            banner_start = (
+                "***************************************************\n"
+                "                FauxPy Started!                    \n"
+                "***************************************************\n"
+            )
+            print("\n")
+            print(banner_start)
+
+            fl_print.normal(f"Running {self._session_object}")
+
+            if (isinstance(self._session_object, FlFamilySession) and
+                    len(self._session_object.get_targeted_failing_test_list()) > 0):
+                fl_print.normal("Targeted failing tests:")
+                for index, item in enumerate(self._session_object.get_targeted_failing_test_list()):
+                    fl_print.normal(f"  {index + 1}. {item}")
+
+            print("\n")
+            banner_dynamic_analysis = (
+                               "==============================\n"
+                               " Dynamic Analysis in Progress \n"
+                               "==============================\n"
+            )
+            print(banner_dynamic_analysis)
+
+            score_entity_list = self._session_object.terminal_summary(
                 terminal_reporter, exitstatus
             )
 
-            delta_time = self._ex_timer.end_timer()
-            self._session_file_manager.save_delta_time_to_file(delta_time)
+            print("\n")
+            print("--- Dynamic Analysis Complete ---")
+
+            session_time = self._session_timer.end()
+            self._session_file_manager.save_delta_time_to_file(session_time)
+
+            for technique, score_list in score_entity_list.items():
+                self._session_file_manager.save_scores_to_file(technique, score_list)
 
             if isinstance(self._session_object, FlFamilySession):
                 fl_session_report = FlSessionReport(
-                    score_entities,
-                    delta_time,
+                    score_entity_list,
+                    session_time,
                     self._session_object.get_fl_granularity(),
                     self._session_object.get_project_working_directory()
                 )
-                fl_session_report.generate_report()
+                report_string = fl_session_report.generate_report()
+                print(report_string)
+
+            banner_end = (
+                "**************************************************\n"
+                "                FauxPy Ended!                     \n"
+                "**************************************************\n"
+            )
+            print(banner_end)
